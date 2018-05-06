fee9f073ac166721c2e8a4f8bad09f2918782b87
grzes77 <grzes77_18@o2.pl>
2018-05-06 09:53:45 +0200

    add env.js

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import router from './router'
 import store from './store'
+import env from './env'
 
 window.axios = axios
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
+axios.defaults.baseURL = env.API_URL
 
 let interval = 0
 
